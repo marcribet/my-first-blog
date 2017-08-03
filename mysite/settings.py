c62799effc81c38c89b8af781513247f015ef83c
Navid <navidbagheri@orange.fr>
2017-08-03 22:25:18 +0200

    d

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#n6e!8gm--kiexs)s^_)66pfgd^e4_ek*@=#bs4$33p%(#y4!k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'marcribet.pythonanywhere.com']
 
